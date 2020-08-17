edd8a124c28ddf58fdb1347d21c54b568125d041
ravi0the0sun <punravi73@gmail.com>
2020-08-17 23:56:17 +1000

    code clean up

--- src/common/config/config.js
+++
@@ -1,6 +0,0 @@
-import { ALCHEMY_API, INFURA_API, ETHERSCAN_API, BUILD } from '@env';
-
-export const ALCHEMY_API_KEY = ALCHEMY_API;
-export const INFURA_API_KEY = INFURA_API;
-export const ETHERSCAN_API_KEY = ETHERSCAN_API;
-export const NETWORK = BUILD !== 'production' ? 'rinkeby' : 'mainnet';
