ea58da0562813481c2d432f33fec705ea01077ae
ravi0the0sun <punravi73@gmail.com>
2020-08-17 23:46:44 +1000

    react native now works

---
+++ src/common/service/ethService.js
@@ -0,0 +1,35 @@
+import Web3 from 'web3';
+
+import {
+	ALCHEMY_API_KEY,
+	INFURA_API_KEY,
+	ETHERSCAN_API_KEY,
+	NETWORK,
+} from '../config/config';
+
+const alchemy_provider = new Web3(
+	new Web3.providers.HttpProvider(
+		`https://eth-${NETWORK}.alchemyapi.io/v2/${ALCHEMY_API_KEY}`
+	)
+);
+const infura_provider = new Web3(
+	new Web3.providers.HttpProvider(
+		`https://${NETWORK}.infura.io/v3/${INFURA_API_KEY}`
+	)
+);
+const local_provider = new Web3(
+	new Web3.providers.HttpProvider(`http://localhost:8545`)
+);
+export function createWallet() {
+	return alchemy_provider.eth.accounts.create();
+}
+
+export async function currentBlock() {
+	return await alchemy_provider.eth.getBlock('latest');
+}
+
+export function privateToWallet() {
+	return alchemy_provider.eth.accounts.privateKeyToAccount(
+		alchemy_provider.utils.randomHex(32)
+	);
+}
