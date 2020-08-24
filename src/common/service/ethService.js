4df9ce5aba2c28eecab7e4c43d01df6e8ca5b477
ravi0the0sun <punravi73@gmail.com>
2020-08-24 13:45:08 +1000

    additional functions

--- src/common/service/ethService.js
+++ src/common/service/ethService.js
@@ -5,6 +5,7 @@ import {
 	INFURA_API_KEY,
 	ETHERSCAN_API_KEY,
 	NETWORK,
+	password,
 } from '../config/config';
 
 const alchemy_provider = new Web3(
@@ -28,6 +29,28 @@ export async function currentBlock() {
 	return await alchemy_provider.eth.getBlock('latest');
 }
 
-export function privateToWallet(pk) {
+export function privateToAccount(pk) {
 	return alchemy_provider.eth.accounts.privateKeyToAccount(pk);
+}
+
+export function encryptAccount(pk) {
+	return JSON.stringify(alchemy_provider.eth.accounts.encrypt(pk, password));
+}
+
+export function decryptAccount(encryptString) {
+	const encryptObj = JSON.parse(encryptString);
+	return alchemy_provider.eth.accounts.decrypt(encryptObj, password);
+}
+
+export function createWallet() {
+	return alchemy_provider.eth.accounts.wallet.create();
+}
+
+export function encryptWallet(wallet) {
+	return JSON.stringify(wallet.encrypt(password));
+}
+
+export function decryptWallet(keystoreString) {
+	const keystoreArray = JSON.parse(keystoreString);
+	return alchemy_provider.eth.accounts.wallet.decrypt(keystoreArray, password);
 }
