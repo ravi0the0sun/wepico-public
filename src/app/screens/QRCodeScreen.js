4a2eb972c51ea7dcd9c92c2ce165633feca14b5c
ravi0the0sun <punravi73@gmail.com>
2020-09-06 21:03:37 +1000

    added clipboard lib

--- src/app/screens/QRCodeScreen.js
+++ src/app/screens/QRCodeScreen.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { Appbar } from 'react-native-paper';
+import { Appbar, Button } from 'react-native-paper';
+import Clipboard from '@react-native-community/clipboard';
 
 import NavBar from '../components/NavBar';
 import QrCode from '../components/QrCode';
+
+let buttonText = 'Copy PrivateKey';
+let buttonIcon = 'clipboard-outline';
 
 export default function QRCodeScreen({ route, navigation }) {
 	const { address } = route.params;
 	const back = (
 		<Appbar.BackAction onPress={() => navigation.navigate('Home')} />
 	);
+	const copyToClipboard = () => {
+		Clipboard.setString(address);
+		buttonIcon = 'clipboard-check-outline';
+		buttonText = 'Copied PrivateKey';
+		setTimeout(() => {
+			buttonText = 'Copy PrivateKey';
+			buttonIcon = 'clipboard-outline';
+		}, 2000);
+	};
 	return (
 		<View>
 			<NavBar title="Recive" backButton={back} />
-            <Text>{address}</Text>
+			<Text>{address}</Text>
 			<QrCode address={address} />
+			<Button
+				color="#ffffff"
+				icon={buttonIcon}
+				style={{ backgroundColor: '#000000' }}
+				onPress={copyToClipboard}>
+				{buttonText}
+			</Button>
 		</View>
 	);
 }
