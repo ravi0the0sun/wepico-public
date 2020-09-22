a5e60c5c051b552060c7031c98a95a27bb07bcb3
ravi0the0sun <punravi73@gmail.com>
2020-09-22 20:10:15 +1000

    using FlatList to render transaction also enabled pull to refresh functionality

--- src/app/components/TransactionBlock.js
+++ src/app/components/TransactionBlock.js
@@ -2,18 +2,11 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { ActivityIndicator } from 'react-native-paper';
 
-import { weiToEth } from '../../common/service/ethService';
-
 export default function TransactionBlock({ showBlock, blockInfo }) {
 	if (!showBlock) {
 		return <></>;
 	}
 
-	const val = blockInfo?.value ? (
-		<Text>ETH: {weiToEth(blockInfo.value)}</Text>
-	) : (
-		<></>
-	);
 	return (
 		<View>
 			{blockInfo ? (
@@ -25,7 +18,6 @@ export default function TransactionBlock({ showBlock, blockInfo }) {
 					<Text>From: {blockInfo.from}</Text>
 					<Text>To: {blockInfo.to}</Text>
 					<Text>Status: {JSON.stringify(blockInfo.status)}</Text>
-					{val}
 				</View>
 			) : (
 				<ActivityIndicator animating={true} color={'#000000'} />
--- src/app/components/TransactionList.js
+++ src/app/components/TransactionList.js
@@ -1,23 +1,47 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import TransactionBlock from './TransactionBlock';
+import { View, Text, FlatList, SafeAreaView } from 'react-native';
+import { List } from 'react-native-paper';
+import { weiToEth } from '../../common/service/ethService';
 
-export default function TransactionList({ transactionList }) {
+const renderItem = ({ item }) => {
+	const { blockNumber, timeStamp, hash, from, to, value } = item;
+	const time = new Date(Number(timeStamp));
+	const eth = weiToEth(value);
+	return (
+		<View>
+			<Text>Block Number: {blockNumber}</Text>
+			<Text>Time: {time.toString()}</Text>
+			<Text>From: {from}</Text>
+			<Text>To: {to}</Text>
+			<Text>Hash: {hash}</Text>
+			<Text>Eth: {eth}eth</Text>
+		</View>
+	);
+};
+
+export default function TransactionList({
+	transactionList,
+	pullToRefresh,
+	refreshing,
+}) {
 	if (!Array.isArray(transactionList)) {
 		return <Text>No Tansaction History</Text>;
 	}
+	function mapping(data, key) {
+		return [...new Map(data.map((x) => [key(x), x])).values()];
+	}
+	const mappedList = mapping(transactionList, (it) => it.hash);
 	return (
 		<View>
-			{transactionList.slice(0, 10).map((blockInfo, index) => {
-				blockInfo.status = blockInfo?.isError === '0' ? true : false;
-				return (
-					<TransactionBlock
-						blockInfo={blockInfo}
-						showBlock={true}
-						key={index}
-					/>
-				);
-			})}
+			<SafeAreaView>
+				<FlatList
+					data={mappedList}
+					renderItem={renderItem}
+					keyExtractor={(item) => item.hash}
+					refreshing={refreshing}
+					onRefresh={() => pullToRefresh()}
+				/>
+			</SafeAreaView>
 		</View>
 	);
 }
