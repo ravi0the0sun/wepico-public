26d31758003c2080fc36e811c161e1bf89f5480a
ravi0the0sun <punravi73@gmail.com>
2020-09-23 10:38:54 +1000

    added paper theme

--- index.js
+++ index.js
@@ -8,9 +8,33 @@ import App from './src/app/App';
 import { name as appName } from './app.json';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
+const theme = {
+	dark: false,
+	roundness: 4,
+	colors: {
+		primary: '#6200ee',
+		accent: '#03dac4',
+		background: '#000000',
+		surface: white,
+		error: '#B00020',
+		text: black,
+		onBackground: '#000000',
+		onSurface: '#000000',
+		disabled: color(black).alpha(0.26).rgb().string(),
+		placeholder: color(black).alpha(0.54).rgb().string(),
+		backdrop: color(black).alpha(0.5).rgb().string(),
+		notification: pinkA400,
+	},
+	fonts: configureFonts(),
+	animation: {
+		scale: 1.0,
+	},
+};
+
 export default function Main() {
 	return (
 		<PaperProvider
+			theme={theme}
 			settings={{
 				icon: (props) => <Ionicons {...props} />,
 			}}>
