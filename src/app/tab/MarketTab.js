c0f59afa9a7873bd07dc7c7d0d3c9a5d8d53ee66
ravi0the0sun <punravi73@gmail.com>
2020-12-18 11:29:12 +1100

    Complete firebase setup

--- src/app/tab/MarketTab.js
+++ src/app/tab/MarketTab.js
@@ -5,10 +5,6 @@ import { Appbar, Button } from 'react-native-paper';
 import NavBar from '../components/NavBar';
 
 export default function MarketTab({ navigation }) {
-	const CoinGeckoClient = new CoinGecko();
-	async function ping() {
-		console.log(await CoinGeckoClient.ping());
-	}
 	return (
 		<>
 			<NavBar title="market" sub={true} />
